0639a6967f99661b9a9ce27ff907cf69ad2d8d1b
rawArtichoke <33503579+rawArtichoke@users.noreply.github.com>
2023-05-01 14:24:18 -0700

    Semi-working Truck and Truck Camera
    YOLO.

--- Assets/Scripts/DriveMode/CameraController_DM.cs
+++ Assets/Scripts/DriveMode/CameraController_DM.cs
@@ -6,58 +6,32 @@ public class CameraController_DM : MonoBehaviour
 {
     // DEFAULT CAMERA POSITION
     // Expected position of camera, relative to truck.
-    public Vector3 defaultPos; 
-    // Expected downward rotation of camera.
-    public float defaultXAngle;
-
-    // CAMERA SMOOTHING TIMES
-    // Smoothing time for camera position
-    public float smoothPosTime;
-    // Smoothing time for camera angle
-    public float smoothAngTime;
-    // Velocity of camera
-
-    // CAMERA VELOCITIES
-    private Vector3 posVelocity = Vector3.zero;
-    // Angle velocity of camera, revolving around the Y-axis.
-    private float yAngleVelocity = 0F;
-
-    // CAMERA OFFSET
-    // Turning pos offset -- How much extra to move because turning.
-    public float posOffset = 0;
-    // Turning ang offset -- How much extra angle to move due to turn.
-    public float angOffset = 0;
-
-    // DESIRED ANGLE
-
-    private float yAngle = 0F;
+    public float defaultPosY;
+    public float defaultPosZ;
+    // Focus of camera
+    private Vector3 focus;
+    // Distance the focus is from the camera.
+    public float focusDist;
 
     // OBJECT TO REFERENCE
     public GameObject playerDrive;
     // Start is called before the first frame update
     void Start()
     {   
-        transform.position = playerDrive.transform.position + defaultPos;
-        transform.Rotate(Vector3.right, defaultXAngle, Space.Self);
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   
         
-        yAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y,
-                        playerDrive.transform.eulerAngles.y, 
-                        ref yAngleVelocity, smoothAngTime);
-        
-        //transform.Rotate(0F, yAngle * Time.deltaTime, 0F, Space.World);
-       
-        transform.position = Vector3.SmoothDamp(transform.position, 
-                             playerDrive.transform.position + defaultPos,
-                             ref posVelocity, smoothPosTime);
-        
-        transform.RotateAround(playerDrive.transform.position, 
-                                Vector3.up, yAngle * Time.deltaTime);
-        
+        Vector3 pdForwardOnXZ = Vector3.ProjectOnPlane(playerDrive.transform.forward, Vector3.up).normalized;
+        Vector3 pdRightOnXZ = Vector3.ProjectOnPlane(playerDrive.transform.right, Vector3.up).normalized;
+
+        transform.position = playerDrive.transform.position + Vector3.up * defaultPosY + pdForwardOnXZ * defaultPosZ;
+
+        focus = playerDrive.transform.position + pdForwardOnXZ * focusDist;
+
+        transform.LookAt(focus);
 
     }
 }
--- Assets/Scripts/DriveMode/PlayerController_DM.cs
+++ Assets/Scripts/DriveMode/PlayerController_DM.cs
@@ -1,34 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController_DM : MonoBehaviour
 {
+    // INPUTS FOR DRIVING
+    public InputActionAsset actions;
+    private InputAction pedalReverseAction;
+    private InputAction steerAction;
+    private InputAction brakeAction;
+
+
+    // Public Game Objects
     public Rigidbody truck;
-    //public Vector3[] forceVectors = new Vector3[4];
+    public GameObject door;
+    public WheelCollider [] tires = new WheelCollider[4];
 
-    /*
-    public float maxSpeed;
-    public float turnSpeed;
-    public float accelSpeed;
+    // Car value maximums
+    public float maxTorque;
     public float maxSteer;
+    public float brakeSpeed;
 
-    private int [] tireState = [0,0,0,0];
-    private int tireIndex;
-    private float tireAngle = 0;
-    private float frontSpeed = 0;
-    private float rearSpeed = 0;
-    */
+    // Real values given
+    private float pedal;
+    private float steer;
+    private float brake;
 
-
-
-
+    //private int [] tireState = [0,0,0,0];
+   
     private int active = 0;
-    private float offset = 5F;
+
+    void Awake()
+    {
+        pedalReverseAction = actions.FindActionMap("Driving").FindAction("Pedal / Reverse");
+
+        steerAction = actions.FindActionMap("Driving").FindAction("Steer");
+
+        brakeAction = actions.FindActionMap("Driving").FindAction("Brake");
+
+        actions.FindActionMap("Driving").FindAction("Package Mode").performed += OnPackage;
+    }
+
+    void OnEnable()
+    {
+        door.SetActive(true);
+        actions.FindActionMap("Driving").Enable();
+    }
+
+    void OnDisable()
+    {
+        door.SetActive(false);
+        actions.FindActionMap("Driving").Disable();
+    }
     // Start is called before the first frame update
     void Start()
     {
-        truck = GetComponent <Rigidbody>();
     }
 
     // Update is called once per frame
@@ -37,6 +64,29 @@ public class PlayerController_DM : MonoBehaviour
     }
     void FixedUpdate()
     {
+        pedal = maxTorque * pedalReverseAction.ReadValue<float>();
+        steer = maxSteer * steerAction.ReadValue<float>();
+        brake = brakeSpeed * brakeAction.ReadValue<float>();
+
+        for (int i = 0; i < 4; i++)
+        {
+            if (i < 2)
+                tires[i].steerAngle = steer;
+            tires[i].motorTorque = pedal;
+
+            tires[i].brakeTorque = brake;
+        }
         
+    }
+
+    private void OnPackage(InputAction.CallbackContext context)
+    {
+        // Somehow communicate with overhead game controller script?
+        return;
+    }
+
+    public Vector3 getXZNormal()
+    {
+        return Vector3.Cross(Vector3.forward, Vector3.right);
     }
 }
